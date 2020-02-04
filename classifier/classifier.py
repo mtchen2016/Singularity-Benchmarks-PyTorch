387609e2c7fd8e419239f1deebe88fe740aca913
rimman <rimman@microsoft.com>
2020-02-03 23:04:02 -0800

    Create classifier.py

---
+++ classifier/classifier.py
@@ -0,0 +1,241 @@
+"""
+Training an Image Classifier
+==================================
+This program does the following in order:
+1. Load and normalize the CIFAR10 training and test datasets using ``torchvision``
+2. Define a Convolutional Neural Network
+3. Define a loss function
+4. Train the network on the training data
+5. Test the network on the test data
+"""
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import time
+import datetime
+
+########################################################################
+# The output of torchvision datasets are PILImage images of range [0, 1].
+# We transform them to Tensors of normalized range [-1, 1].
+# note:
+#     If running on Windows and get a BrokenPipeError, try setting
+#     the num_worker of torch.utils.data.DataLoader() to 0.
+
+########################################################################
+# 1. Loading and normalizing CIFAR10
+########################################################################
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', 
+                                        train=True,
+                                        download=True, 
+                                        transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, 
+                                          batch_size=4,
+                                          shuffle=True, 
+                                          num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', 
+                                       train=False,
+                                       download=True, 
+                                       transform=transform)
+
+testloader = torch.utils.data.DataLoader(testset, 
+                                         batch_size=4,
+                                         shuffle=False, 
+                                         num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+########################################################################
+# 2. Define a Convolutional Neural Network
+########################################################################
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+########################################################################
+# 3. Define a Loss function and optimizer
+########################################################################
+# Let's use a Classification Cross-Entropy loss and SGD with momentum.
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+########################################################################
+# 4. Train the network
+########################################################################
+#
+# This is when things start to get interesting.
+# We simply have to loop over our data iterator, and feed the inputs to the
+# network and optimize.
+
+training_start = time.time()
+    
+for epoch in range(2):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        start = time.time()
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        batch_time = time.time() - start
+        
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[Epoch %d, iteration(s) %5d] loss: %.3f training time: %.5f' %
+                  (epoch + 1, i + 1, running_loss / 2000, batch_time))
+            running_loss = 0.0
+
+elapse_time = time.time() - training_start
+elapse_time = datetime.timedelta(seconds=elapse_time)
+print("Finished Training. Total training time {}".format(elapse_time))
+
+########################################################################
+# Save the trained model
+########################################################################
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+########################################################################
+# 5. Test the network on the test data
+########################################################################
+#
+# We have trained the network for 2 passes over the training dataset.
+# But we need to check if the network has learnt anything at all.
+#
+# We will check this by predicting the class label that the neural network
+# outputs, and checking it against the ground-truth. If the prediction is
+# correct, we add the sample to the list of correct predictions.
+#
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# print images
+#imshow(torchvision.utils.make_grid(images))
+print()
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+########################################################################
+# Next, let's load back in our saved model (note: saving and re-loading the model
+# wasn't necessary here, we just did it to validate that we can do so):
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
+
+########################################################################
+# Let's see what the neural network thinks these examples above are:
+
+outputs = net(images)
+
+########################################################################
+# The outputs are energies for the 10 classes.
+# The higher the energy for a class, the more the network
+# thinks that the image is of the particular class.
+# So, let's get the index of the highest energy:
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+
+########################################################################
+# The results seem pretty good.
+#
+# Let us look at how the network performs on the whole dataset.
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print()
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+########################################################################
+# That looks way better than chance, which is 10% accuracy (randomly picking
+# a class out of 10 classes).
+# Seems like the network learnt something.
+#
+# Hmmm, what are the classes that performed well, and the classes that did
+# not perform well:
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+
+########################################################################
+# Training on GPU
+# ----------------
+# Just like how you transfer a Tensor onto the GPU, you transfer the neural
+# net onto the GPU.
+#
+# Let's first define our device as the first visible cuda device if we have
+# CUDA available:
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+
+print(device)
+
+
+del dataiter
